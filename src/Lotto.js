9f52502dd8bc9319dea71cc58613bdbb1617de0e
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-11-01 13:19:42 +0900

    feat : Lotto 클래스 객체 예외처리 규칙 정의

--- src/Lotto.js
+++ src/Lotto.js
@@ -10,9 +10,22 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
     }
+
+    const unique = new Set(numbers);
+    if (unique.size !== 6) {
+      throw new Error("[ERROR] 중복된 번호가 있습니다.");
+    }
+
+    numbers.array.forEach((numbers) => {
+      if (numbers < 1 || numbers > 45) {
+        throw new Error("[ERROR] 번호는 1~45 사이여야 합니다.");
+      }
+    });
   }
 
-  // TODO: 추가 기능 구현
+  getNumbers() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
